32390a4be3ffa8993779121eefbaaa92cc921bfa
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-07 19:20:49 -0300

    Aula 16 - Configurando sqlAlchemy.

--- 02-gerenciamento/app.py
+++ 02-gerenciamento/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABSE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
