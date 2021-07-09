fa038fcbbd7dc4e04962f01bcaed60619128d645
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-08 23:53:55 -0300

    Aula 36 - Iterando objetos.

--- template-engine/app.py
+++ template-engine/app.py
@@ -5,12 +5,12 @@ app = Flask(__name__,template_folder="tema")
 
 @app.route("/templates")
 def templates():
-    user = {
+    users = {
         "name": "Lucas Silva",
         "idade": 99,
         "email": "lucas@teste.com.br"
     }
-    return render_template("index.html",user=user)
+    return render_template("index.html",users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
