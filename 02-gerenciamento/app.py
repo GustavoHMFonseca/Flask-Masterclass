450462c4a7751ad08bbee1a08fafdec337ab3d40
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-09 19:20:54 -0300

    Aula 54 - Permanecer conectado.

--- 02-gerenciamento/app.py
+++ 02-gerenciamento/app.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from flask import Flask,render_template,redirect,request,url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_manager,login_required,login_user,logout_user
@@ -78,7 +79,7 @@ def login():
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
-
+        remember = request.form["remember"]
         user = User.query.filter_by(email=email).first()
        
         
@@ -90,7 +91,7 @@ def login():
             flash("Senha incorreta")
             return redirect(url_for("login"))
 
-        login_user(user)
+        login_user(user,remember = remember, duration=timedelta(days=7))
         return redirect(url_for("index"))
 
     return render_template("login.html")
