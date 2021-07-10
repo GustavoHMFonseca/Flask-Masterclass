51ca730ff0523f080146e891c1136a5701dafdec
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-10 20:52:55 -0300

    Aula 81 - Criando testes para Flask

--- 07-teste/tests/test_app.py
+++ 07-teste/tests/test_app.py
@@ -1,9 +1,21 @@
+from click.core import Context
 import pytest
+from app import create_app
 
 @pytest.fixture
 def cliente():
-    return "ACESSANDO A PÁGINA INDEX.HTML"
+    app = create_app()
+    app.config["TESTING"] = True
+
+    context = app.app_context()
+    context.push()
+
+    yield app.test_client()
+
+    context.pop()
+    
 
 
-def test_client(cliente):
-    assert cliente == "ACESSANDO A PÁGINA INDEX.HTMLS"
+def test_se_a_pagina_de_usuarios_retorna_status_code_200(cliente):
+   response = cliente.get("/")
+   assert response.status_code == 200
