abc666516d9ca6cda5048d8f79480e009f3324c5
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-10 01:02:07 -0300

    Aula 63 - Válidando Forms.

--- 05 - forms/app/forms.py
+++ 05 - forms/app/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
 from wtforms.fields.html5 import EmailField
 from wtforms.fields import PasswordField, BooleanField, SubmitField
+from wtforms.validators import Length, Email
+
 
 class LoginForm(FlaskForm):
-    email = EmailField("Email")
-    password = PasswordField("senha")
+    email = EmailField("Email", validators=[Email()])
+    password = PasswordField("senha", validators=[Length(3,6,"Sua senha deve conter de 3 a 6 cxaracteres.")])
     remember = BooleanField("Permanecer Conectado")
     submit = SubmitField("Logar")
