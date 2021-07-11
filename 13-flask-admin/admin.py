662cf26761d62b614f6321a251f8efe55467841e
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-11 19:25:02 -0300

    Aula 95 - Adicionando Search bar.

--- 13-flask-admin/admin.py
+++ 13-flask-admin/admin.py
@@ -1,10 +1,15 @@
+from re import search
 from flask_admin.contrib.sqla import ModelView
 
 from models import User
 from app import db
 
 class UserView(ModelView):
-    pass
+    column_editable_list = ("name","email")
+    form_edit_rules={"name","email"}
+    column_searchable_list = ["email"]
+    edit_modal=True
+    
 
 def init_app(admin):
     admin.add_view(UserView(User, db.session))
