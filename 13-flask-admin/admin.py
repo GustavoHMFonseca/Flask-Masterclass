bd172afe8519e33f4374be15654ffe307669b408
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-11 19:31:28 -0300

    Aula 96 - Campo tipo senha.

--- 13-flask-admin/admin.py
+++ 13-flask-admin/admin.py
@@ -1,5 +1,7 @@
 from re import search
 from flask_admin.contrib.sqla import ModelView
+from wtforms.fields import PasswordField
+from werkzeug.security import generate_password_hash
 
 from models import User
 from app import db
@@ -9,6 +11,10 @@ class UserView(ModelView):
     form_edit_rules={"name","email"}
     column_searchable_list = ["email"]
     edit_modal=True
+
+    form_extra_fields={
+        "password": PasswordField("Password")
+    }
     
 
 def init_app(admin):
