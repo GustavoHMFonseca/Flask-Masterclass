d77d0f66a12c9cabf0d36f91799c995958d0ee72
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-10 22:42:45 -0300

    AUla 87 - Enviando e-mail.

--- 08-email/app.py
+++ 08-email/app.py
@@ -5,7 +5,7 @@ from flask_mail import Mail, Message
 config ={
     "MAIL_SERVER" : "smtp.ethereal.email",
     "MAIL_PORT" : 587,
-    "MAIL_TLS" : True,
+    "MAIL_USE_TLS" : True,
     "MAIL_DEBUG" : True,
     "MAIL_USERNAME" : "dagmar.swift@ethereal.email",
     "MAIL_PASSWORD" : "WxbgxbGRmmHs12ejGZ",
@@ -13,12 +13,15 @@ config ={
 }
 
 app = Flask(__name__)
+app.config.update(config)
 mail = Mail(app)
 
 
 @app.route("/sendmail")
 def sendmail():
-    msg = Message()
+    msg = Message(subject="Bem-vindo(a)", sender=app.config["MAIL_DEFAULT_SENDER"], recipients=["amanda@teste.com.br"],body="Apenas mais um e-mail enviado de um smtp faker.")
+    mail.send(msg)
+
     return "Email enviado com sucesso!"
 
 if __name__ == "__main__":
