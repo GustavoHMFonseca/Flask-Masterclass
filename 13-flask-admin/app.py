73c999c8b2f45c208bc82cf63a69ad161628a074
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-11 18:14:26 -0300

    Aula 91 - Criando Models.

--- 13-flask-admin/app.py
+++ 13-flask-admin/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["FLASK_ADMIN_SWATCH"] = "cosmo"
-Admin(app, name="SpaceDevs", template_mode="bootstrap3")
+app.config["FLASK_ADMIN_SWATCH"] = "cosmo" 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dev.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+admin = Admin(app, name="SpaceDevs", template_mode="bootstrap3")
+db = SQLAlchemy(app)
+
+
+from models import User
+
+class UserView(ModelView):
+    pass
+
+admin.add_view(UserView(User, db.session))
 
 app.run(debug=True)
