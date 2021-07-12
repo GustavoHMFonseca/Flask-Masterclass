d4655f0a5ebe91a6985362ce163d9dedeee9cf76
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-07-12 09:51:55 -0300

    Aula 108 - Configurando Banco de dados.

--- statusok/flask-contacts/config.py
+++ statusok/flask-contacts/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = "SECRET"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///teste.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgre:admin@localhost/flask_contacts"
     SQLALCHEMY_TRACK_MODIFICATION = False
 
 class Development(Config):
